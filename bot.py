39877862bf5ab86f3fd48625c15e9dd8004bd621
Bottersnike <bottersnike237@gmail.com>
2018-09-28 10:21:45 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,10 @@
+from cogs.util.bot import VEXBot
+
+
+def main():
+    bot = VEXBot()
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ cogs/vex.py
@@ -0,0 +1,84 @@
+import subprocess
+import asyncio
+import inspect
+import sys
+
+from discord.ext import commands
+import ruamel.yaml as yaml
+import discord
+
+from .util.checks import right_channel, is_developer, is_owner
+
+
+SELF_SETTING = {
+    'y10': 494761507004088320,
+    'y12': 494761657734791189,
+    'builder': 494525550178729995,
+    'coder': 494525300500201482,
+    'mechanic': 494525175337844736,
+    'driver': 494524660763852803,
+}
+
+
+class VEX:
+    """Core commands"""
+    async def __local_check(self, ctx):
+        return right_channel(ctx)
+
+    @commands.command()
+    @is_developer()
+    async def list_roles(self, ctx):
+        if ctx.guild is None:
+            return await ctx.send('Must be used in a guild')
+        roles = '```'
+        for role in ctx.guild.roles:
+            roles += f'{role.name}: {role.id}\n'
+        roles += '```'
+        await ctx.send(roles)
+
+    @commands.command()
+    async def iam(self, ctx, *, role):
+        if role not in SELF_SETTING:
+            e = discord.Embed(title='No role found under that name', color=0xbb5555)
+            await ctx.send(embed=e, delete_after=5)
+            return await ctx.message.delete()
+        
+        role_id = SELF_SETTING[role]
+        role = discord.utils.get(ctx.guild.roles, id=role_id)
+        
+        await ctx.author.add_roles(role)
+        e = discord.Embed(title='Added role succesfully!', color=0x55bbbb)
+        await ctx.send(embed=e, delete_after=5)
+        await ctx.message.delete()
+    
+    @commands.command()
+    async def iamnot(self, ctx, *, role):
+        if role not in SELF_SETTING:
+            e = discord.Embed(title='No role found undr that name', color=0xbb5555)
+            await ctx.send(embed=e, delete_after=5)
+            return await ctx.message.delete()
+            
+        role_id = SELF_SETTING[role]
+        role = discord.utils.get(ctx.guild.roles, id=role_id)
+        
+        await ctx.author.remove_roles(role)
+        e = discord.Embed(title='Removed role succesfully!', color=0x55bbbb)
+        await ctx.send(embed=e, delete_after=5)
+        await ctx.message.delete()
+    
+    @commands.command()
+    async def roles(self, ctx):
+        if ctx.guild is None:
+            return
+
+        e = discord.Embed(title='Roles setup for self-service:', color=0xffff00)
+        for i in SELF_SETTING:
+            role = discord.utils.get(ctx.guild.roles, id=SELF_SETTING[i])
+            if role is not None:
+                e.add_field(name=role.name, value=f'`{ctx.prefix}iam {i}`')
+        
+        await ctx.send(embed=e)
+
+        
+def setup(bot):
+    bot.add_cog(VEX())
