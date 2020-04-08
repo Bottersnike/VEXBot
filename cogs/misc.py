e7909903be9385f090bcda147c5ccb9c88078f5d
Bottersnike <bottersnike237@gmail.com>
2020-04-09 00:39:31 +0100

    Add faq

--- cogs/misc.py
+++ cogs/misc.py
@@ -25,6 +25,9 @@ The accuracy of the predictions is only as good as that model.
 Each team has two values associated with it. The first value is based on the average score that team achieves. The second value is based on the average proporition of points the opponoent team scores proporitional to the difference in scores and the average score for that whole competion.
 Predicting the results for a match is a case of summing the first value, then subtracting a multiplier of the second, all weighted according to the number of teams in an alliance.
 """.strip(), inline=False)
+        e.add_field(name="Why does a team higher on the leaderboard score worse against one lower?", value="""
+The leaderboard currently only takes into account the first of these two values, while score predictions take into account both. I'm working on fixing that at some point.
+""".strip(), inline=False)
 
         await ctx.send(embed=e)
 
